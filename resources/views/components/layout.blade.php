1b9fb62c6b747e78bc0e9b42806e4176a8a32874
Lucas Santana <lucasdeoliveirasantana874@gmail.com>
2024-06-26 21:02:51 -0300

    instalando bootstrap e estilizando as views

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,11 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} - Controle de Series</title>
+    <link rel="stylesheet" href="{{ asset("css/app.css") }}">
 </head>
 
 <body>
-    <h1>{{ $title }}</h1>
-    {{ $slot }}
+    <div class="container">
+        <h1>{{ $title }}</h1>
+        {{ $slot }}
+    </div>
 </body>
 
 </html>
